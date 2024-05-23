0286ad982b05981fd7016f09e58030571dedc2ed
sujit <sujitshrestha0004@gmail.com>
2024-05-23 14:33:18 +0545

    role using ajax

--- wp-content/plugins/role_using_ajax/autoloader.php
+++ wp-content/plugins/role_using_ajax/autoloader.php
@@ -10,8 +10,7 @@ namespace Plugins\Role_using_ajax;
   //exploding the class name in case the class name consists of namespaces
   //then , reversing the array and taking the 0th part so that it complies with the case when the namespace is not presented
   $classname =dirname(__FILE__) .'\\'. lcfirst(array_reverse(explode( '\\' , $class ))[0]) . '.php';
-  //   echo $classname;
-  // exit;
+
   if( file_exists( $classname ) ){
 
     require_once $classname ;
